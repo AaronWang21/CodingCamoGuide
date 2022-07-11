d08d4d9d441858d217f06f8a3c042f2fd3c6dba9
AaronWang21 <108958941+AaronWang21@users.noreply.github.com>
2022-07-11 14:56:08 -0700

    Updated peak challenge code

--- Week 1/src/arraysChallenge/Peak.java
+++ Week 1/src/arraysChallenge/Peak.java
@@ -1,11 +1,20 @@
 package arraysChallenge;
 public class Peak {
     public static void main(String[] args) {
-        int[] integerArray = {5,6,2,7,10,2,-1,5};
-        for(int i = 0; i < integerArray.length; i++){
-            if(i - 1 > 0 && i + 1 < integerArray.length && integerArray[i - 1] < integerArray[i] && integerArray[i] > integerArray[i + 1]){
+        int[] arr = {1,2,3};
+        if(arr.length == 1){
+            System.out.println("0");
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(i - 1 >= 0 && i + 1 < arr.length && arr[i - 1] < arr[i] && arr[i] > arr[i + 1]){
                 System.out.println(i);
                 break;
+            }
+            else if(i == 0 && arr[i] > arr[i + 1]){
+                System.out.println(i);
+            }
+            else if(i == arr.length - 1 && arr[i] > arr[i - 1]){
+                System.out.println(i);
             }
         }
     }
